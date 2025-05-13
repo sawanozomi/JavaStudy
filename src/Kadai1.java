d20ac1e2550202ba986f1ecd1e018cfa145fecbc
302A-01\cre <a>
2025-05-13 16:07:20 +0900

    エラーメッセージ

--- src/Kadai1.java
+++ src/Kadai1.java
@@ -2,7 +2,13 @@
 public class Kadai1 {
     public static void main(String[] args) {
 
-        int num1 = Integer.parseInt (args[0]);
+        num1 = 
+        try {
+            int num1 = Integer.parseInt (args[0]);
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("引数が設定されていません") ;
+        }
+
         int count = 0;
 
         for (int i = 0; i <= 1000; i++){
