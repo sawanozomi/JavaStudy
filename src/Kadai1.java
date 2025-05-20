99d43be127cf0a929279223eb7ceaae8cf3cfc33
302A-01\cre <a>
2025-05-20 13:45:01 +0900

    kadai1 終わり

--- src/Kadai1.java
+++ src/Kadai1.java
@@ -2,16 +2,20 @@
 public class Kadai1 {
     public static void main(String[] args) {
 
-        num1 = 
+        int num1 = 0;
         try {
-            int num1 = Integer.parseInt (args[0]);
+            num1 = Integer.parseInt(args[0]);
         } catch (ArrayIndexOutOfBoundsException e) {
             System.out.println("引数が設定されていません") ;
+            return;
+        } catch (NumberFormatException e){
+            System.out.println("引数が整数ではありません");
+            return;
         }
 
         int count = 0;
 
-        for (int i = 0; i <= 1000; i++){
+        for (int i = 1; i <= 1000; i++){
             System.out.print (i);
             count ++;
             if (count == num1){
