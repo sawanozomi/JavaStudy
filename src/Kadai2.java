a1e14833cf4eef72f4ea1232cbade123db24a62e
302A-01\cre <a>
2025-06-03 15:14:48 +0900

    kadai2 3kara

--- src/Kadai2.java
+++ src/Kadai2.java
@@ -1,6 +1,24 @@
+import java.io.*;
+import java.nio.charset.Charset;
+import java.nio.file.Path;
+
 public class Kadai2 {
     public static void main(String[] args) {
-        
+        try (
+            BufferedReader br = new BufferedReader(
+                new FileReader(
+                        "src/BookInfo.csv",
+                        Charset.forName("MS932")
+                )
+            )
+        ) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                System.out.println(line);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
 }
