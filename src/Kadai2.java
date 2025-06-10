0a0d12846638d25af93491b441f956f848e76dc2
302A-01\cre <a>
2025-06-10 15:12:34 +0900

    kadai2 book.java nanka

--- src/Kadai2.java
+++ src/Kadai2.java
@@ -1,24 +1,37 @@
 import java.io.*;
 import java.nio.charset.Charset;
-import java.nio.file.Path;
+import java.util.ArrayList;
 
 public class Kadai2 {
     public static void main(String[] args) {
-        try (
-            BufferedReader br = new BufferedReader(
-                new FileReader(
-                        "src/BookInfo.csv",
-                        Charset.forName("MS932")
-                )
-            )
-        ) {
-            String line;
-            while ((line = br.readLine()) != null) {
-                System.out.println(line);
+        try {
+            ArrayList<String> readLines = fileRead("src/BookInfo.csv");
+
+            for (String s : readLines) {
+                System.out.println(s);
             }
-        } catch (Exception e) {
-            e.printStackTrace();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
     }
 
+    private static ArrayList<String> fileRead(String filePath) throws IOException {
+        BufferedReader br = new BufferedReader(
+                new FileReader(
+                        filePath,
+                        Charset.forName("MS932")
+                )
+        );
+        String line;
+        while ((line = br.readLine()) != null) {
+           // System.out.println(line);
+        }
+
+        ArrayList<String> list = new ArrayList<>();
+        list.add("title");
+        list.add("author");
+        list.add("loanDate");
+        list.add("returnDate");
+        return list;
+    }
 }
